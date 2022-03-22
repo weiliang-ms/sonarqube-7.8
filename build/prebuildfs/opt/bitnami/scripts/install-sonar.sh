43da3bdfbdf0b3f03586f875fedbe5da073b2944
weiliang <1067599390@qq.com>
2022-03-22 15:30:42 +0800

    add sonar 7.8

--- build/prebuildfs/opt/bitnami/scripts/install-sonar.sh
+++ build/prebuildfs/opt/bitnami/scripts/install-sonar.sh
@@ -10,8 +10,6 @@ set -x \
     && curl -o sonarqube.zip -fSL https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-$SONAR_VERSION.zip \
     && unzip -q sonarqube.zip \
     && mv sonarqube-$SONAR_VERSION sonarqube \
-    && chown -R sonarqube:sonarqube sonarqube \
-    && chmod -R 0644 sonarqube \
     && rm sonarqube.zip*
 
     # todo verify
