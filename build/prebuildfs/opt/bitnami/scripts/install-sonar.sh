f8022a48e5756f3fa1666206bc0fd5834ce5dbd0
weiliang <1067599390@qq.com>
2022-03-22 14:39:45 +0800

    add sonar 7.8

---
+++ build/prebuildfs/opt/bitnami/scripts/install-sonar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://github.com/SonarSource/docker-sonarqube/blob/master/7/community/Dockerfile
+
+export SONAR_VERSION=7.8
+groupadd -r sonarqube && useradd -r -g sonarqube sonarqube
+
+set -x \
+    && cd /opt/bitnami \
+    && curl -o sonarqube.zip -fSL https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-$SONAR_VERSION.zip \
+    && curl -o sonarqube.zip.asc -fSL https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-$SONAR_VERSION.zip.asc \
+    && gpg --batch --verify sonarqube.zip.asc sonarqube.zip \
+    && unzip -q sonarqube.zip \
+    && mv sonarqube-$SONAR_VERSION sonarqube \
+    && chown -R sonarqube:sonarqube sonarqube \
+    && rm sonarqube.zip*
